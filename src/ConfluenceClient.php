ba10c889b469a3ff3b698ef2e8fa72af4d10dee7
lxxps <pierrotevrard@gmail.com>
2023-02-21 12:38:51 +0100

    Add "path" and "userInfo" as HTTP Client Builder plugins
    Also mark some properties as "protected" for convenience

--- src/ConfluenceClient.php
+++ src/ConfluenceClient.php
@@ -8,15 +8,25 @@ use CloudPlayDev\ConfluenceClient\HttpClient\Builder;
 use CloudPlayDev\ConfluenceClient\HttpClient\Plugin\TokenAuthentication;
 use Http\Client\Common\HttpMethodsClientInterface;
 use Http\Client\Common\Plugin\AddHostPlugin;
+use Http\Client\Common\Plugin\AddPathPlugin;
+use Http\Client\Common\Plugin\AuthenticationPlugin;
 use Http\Client\Common\Plugin\HeaderDefaultsPlugin;
+use Http\Message\Authentication\BasicAuth;
 
 class ConfluenceClient
 {
     /**
      * Default User-Agent send with every request
+     * Use protected visibility to facilitate extension
+     * @see http://fabien.potencier.org/pragmatism-over-theory-protected-vs-private.html
      */
-    private const USER_AGENT = 'cloudplaydev-confluence-php-client/0.1.0';
-    private Builder $httpClientBuilder;
+    protected const USER_AGENT = 'cloudplaydev-confluence-php-client/0.1.0';
+    
+    /**
+     * Use protected visibility to facilitate extension
+     * @see http://fabien.potencier.org/pragmatism-over-theory-protected-vs-private.html
+     */
+    protected Builder $httpClientBuilder;
 
     public function __construct(string $confluenceHostUrl, Builder $httpClientBuilder = null)
     {
@@ -51,13 +61,31 @@ class ConfluenceClient
         return $this->httpClientBuilder->getHttpClient();
     }
 
+    /**
+     * Register basic properties about original URI in order to
+     * restore them on every request
+     *
+     * @param string $url
+     * @return void
+     */
     public function setUrl(string $url): void
     {
         $uri = $this->httpClientBuilder->getUriFactory()->createUri($url);
 
         $this->httpClientBuilder->removePlugin(AddHostPlugin::class);
         $this->httpClientBuilder->addPlugin(new AddHostPlugin($uri));
+
+        // Prepend path if any
+        if( $uri->getPath() ) {
+            $this->httpClientBuilder->removePlugin(AddPathPlugin::class);
+            $this->httpClientBuilder->addPlugin(new AddPathPlugin($uri));
+        }
+
+        // Report userInfo as Basic authentication
+        if( $uri->getUserInfo() ) {
+            list( $user , $token ) = explode( ':' , $uri->getUserInfo() , 2 );
+            $this->httpClientBuilder->removePlugin(AuthenticationPlugin::class);
+            $this->httpClientBuilder->addPlugin(new AuthenticationPlugin( new BasicAuth( $user , $token ) ) );
+        }
     }
-
-
 }
