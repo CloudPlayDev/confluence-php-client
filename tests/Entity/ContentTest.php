c42dd253a17820a2ef6a8176262e53ccf22ba95d
virtualLast <mjlast@gmail.com>
2026-07-15 17:46:58 +0100

    tests for absract content + search result content loading the new attachment

--- tests/Entity/ContentTest.php
+++ tests/Entity/ContentTest.php
@@ -9,7 +9,11 @@ declare(strict_types=1);
 
 namespace CloudPlayDev\Tests\ConfluenceClient\Entity;
 
+use CloudPlayDev\ConfluenceClient\Entity\AbstractContent;
+use CloudPlayDev\ConfluenceClient\Entity\ContentAttachment;
 use CloudPlayDev\ConfluenceClient\Entity\ContentPage;
+use CloudPlayDev\ConfluenceClient\Entity\ContentSearchResult;
+use CloudPlayDev\ConfluenceClient\Exception\HydrationException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -44,6 +48,39 @@ class ContentTest extends TestCase
         $confluencePage = new ContentPage();
         $confluencePage->setId(123);
         self::assertSame(123, $confluencePage->getId());
+    }
+
+    public function testLoadAttachment(): void
+    {
+        $attachment = AbstractContent::load(self::attachmentPayload());
+
+        self::assertInstanceOf(ContentAttachment::class, $attachment);
+        self::assertSame('attachment', $attachment->getType());
+    }
+
+    public function testSearchResultLoadsAttachment(): void
+    {
+        $searchResult = ContentSearchResult::load([
+            'size' => 1,
+            'results' => [self::attachmentPayload()],
+        ]);
+
+        self::assertInstanceOf(ContentAttachment::class, $searchResult->getResultAt(0));
+    }
+
+    /**
+     * @return mixed[]
+     */
+    private static function attachmentPayload(): array
+    {
+        return [
+            'id' => '123456',
+            'type' => 'attachment',
+            'title' => 'example.pdf',
+            '_links' => [
+                'self' => 'https://example.test/rest/api/content/123456',
+            ],
+        ];
     }
 
 }
